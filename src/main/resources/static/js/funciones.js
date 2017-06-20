b52f6c674bd3c2b71bf244fe7026cc959b377932
VeronicaPerez <veronicapr88@gmail.com>
2017-06-20 00:53:45 -0300

    se agregan nuevas funciones javascrips

--- src/main/resources/static/js/funciones.js
+++ src/main/resources/static/js/funciones.js
@@ -1,3 +1,162 @@
+function formatDate() {
+				          var date = new Date();
+				          var month = date.getMonth() + 1;							        
+				          if (month < 10)
+				          	 month= "0" + month;
+				          return date.getDate() + "/" + month + "/" + date.getFullYear();
+			      	}
+// inicia el calendario(index.jsp)
+function cargaCalendario() {
+	    $('#calendar').fullCalendar({
+	    })
+}
+
+//carga data table de espectaculo (index.jsp)
+function cargaDatatable(){
+	  
+		var table= $('#espectaculo').dataTable( {
+	    "ajax": {
+	           "url": '/verProximosEspectaculosYSusRealizaciones/',
+	           "dataType": 'json',
+	           "dataSrc": "content",
+	           "type": "GET",
+	           "data": {
+						"_start":"1",
+						"_end":"1000"
+	                   },
+	           "beforeSend": function(xhr){
+	                   var pathname = window.location.pathname;
+	                   xhr.setRequestHeader("X-TenantID", pathname.split('/')[1]);
+	           }
+	    },		            
+	    "columns" :  [ 
+	          { "data": "id"},
+			  { "data": "realizacionEspectaculo.0.fecha",
+		    		"render": function (data) {
+				          var date = new Date(data);
+				          var month = date.getMonth() + 1;							        
+				          if (month < 10)
+				          	 month= "0" + month;
+				          return date.getDate() + "/" + month + "/" + date.getFullYear();
+			      	}
+				},
+	            { "data": "nombre",
+	              "render": function ( data, type, full ) {
+	            	  return '<a href="infoEspectaculo?id=' + full['id'] + '">' + data + '</a>';
+	            	  //return '<a href="infoEspectaculo">' + data + '</a>';
+	               }
+	            },
+	            { "data": "realizacionEspectaculo.0.sala.nombre" },    
+	            { "data": "realizacionEspectaculo.0.sala.direccion" }
+	         
+	     ],		                     
+	    "columnDefs": [ 
+	        { "visible": false, "targets": [0] }
+	                    ],
+		});
+	}
+
+//toma de la url el id del espectaculo(infoEspectaculo.jsp)
+function getGET()
+{
+    // capturamos la url
+    var loc = document.location.href;
+    // si existe el interrogante
+    if(loc.indexOf('?')>0)
+    {
+        // cogemos la parte de la url que hay despues del interrogante
+        var getString = loc.split('?')[1];
+        // obtenemos un array con cada clave=valor
+        var GET = getString.split('&');
+        var get = {};
+
+        // recorremos todo el array de valores
+        for(var i = 0, l = GET.length; i < l; i++){
+            var tmp = GET[i].split('=');
+            get[tmp[0]] = unescape(decodeURI(tmp[1]));
+        }
+        return get;
+    }
+}
+
+//carga la info del espectaculo y sus realizaciones(infoEspectaculo.jsp)
+function cargaInfoEspectaculo(){
+	var valores=getGET();
+	
+	 $.ajax({
+	        url: "/verEspectaculoYSusRealizaciones/",
+	 		dataType: "json",
+     		dataSrc: "",
+     		type: "GET",
+     		data: {
+				idEspectaculo: valores["id"]
+             },
+             beforeSend: function(xhr){
+                 var pathname = window.location.pathname;
+                 xhr.setRequestHeader("X-TenantID", pathname.split('/')[1]);
+         }
+	    }).then(function(data) {
+    	   $("#evento").append(data.nombre);
+    	   $("#nomEspectaculo").append(data.nombre);
+    	   $("#descripcion").append(data.descripcion);
+    	   $("#tipoEspectaculo").append(data.tipoEspectaculo[0].nombre);
+	       $("#sala").append(data.realizacionEspectaculo[0].sala.nombre);
+	       $("#direccion").append(data.realizacionEspectaculo[0].sala.direccion);
+	       $("#localidades").append(data.realizacionEspectaculo[0].sala.total_localidad);
+	       $("#lugarMapa").append(data.realizacionEspectaculo[0].sala.nombre);
+//	       $("#sector").append(data[0].sectores[0].nombre);
+//	       $("#precioSector").append(data[0].sectores[0].precio);
+	   
+	       //Envia direccion al mapa
+	       var direccion = data.realizacionEspectaculo[0].sala.direccion;
+	   		// Creamos el Objeto Geocoder
+	       var geocoder = new google.maps.Geocoder();
+	       //se envia la direccion y se invoca la funcion
+	       geocoder.geocode({ 'address': direccion}, geocodeResult);
+	       
+	       for (index in data.realizacionEspectaculo) {
+				var fecha = data.realizacionEspectaculo[index].fecha;
+				var date = new Date(fecha);
+				var month = date.getMonth() + 1;							        
+				if (month < 10) {
+					 month= "0" + month;
+				}
+				var nuevafecha = date.getDate() + "/" + month + "/" + date.getFullYear();
+				
+				// Si hay mas de una fecha agrega un guion "-" separador
+				if (index > 0)
+					nuevafecha = " - " + nuevafecha;
+					
+				$("#fecha").append(nuevafecha);
+	       }
+	     });
+	
+     }
+
+//carga el mapa y genera coordenadas para el
+function geocodeResult(results, status) {
+    // Verificamos el estatus
+    if (status == 'OK') {
+       
+        var mapOptions = {
+            center: results[0].geometry.location,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        mapC = new google.maps.Map($("#map").get(0), mapOptions);
+        // fitBounds acercará el mapa con el zoom adecuado de acuerdo a lo buscado
+        mapC.fitBounds(results[0].geometry.viewport);
+        // Dibujamos un marcador con la ubicación del primer resultado obtenido
+        var markerOptions = { position: results[0].geometry.location }
+        var marker = new google.maps.Marker(markerOptions);
+        marker.setMap(mapC);
+    } else {
+        
+        alert("Geocoding no tuvo éxito debido a: " + status);
+    }
+}
+	
+
+//funcion validar usuario y contraseña(login.jsp)
 function valida() {
 		var email = document.getElementById('email');
 		var password = document.getElementById("password");
